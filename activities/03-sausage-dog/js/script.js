68941a684f5a39999af45b69d991026d3bb5623c
Julie <gulshan.khashimova@gmail.com>
2022-01-14 15:57:32 -0500

    A1: finished the main part of the acrivity

--- activities/03-sausage-dog/js/script.js
+++ activities/03-sausage-dog/js/script.js
@@ -1,33 +1,50 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+const ANIMAL_IMG = 10;
+const NUM_ANIMALS = 100;
 
-/**
-Description of preload
-*/
+let animalImages = []; //stores animal images
+let animals = []; //stores animal objects
+
+let sausageDogImg;
+let sausageDog;
+
 function preload() {
-
+  for (let i = 0; i < ANIMAL_IMG; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+   sausageDogImg = loadImage(`assets/images/sausage-dog.png`);
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // create the animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+
+// create the sausageDog
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImg);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed(){
+  sausageDog.mousePressed();
 }
