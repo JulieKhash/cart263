cd5ef1d6814dec0ece154654ab7c4fcdcc48ccd9
Julie <gulshan.khashimova@gmail.com>
2022-01-14 20:12:48 -0500

    E1: preloaded an array of  butterfly images

--- exercises/sausage-dog-new-game/js/script.js
+++ exercises/sausage-dog-new-game/js/script.js
@@ -8,10 +8,19 @@ author, and this description to match your project!
 
 "use strict";
 
+const BUTTERFLY_IMG = 14;
+const NUM_BUTTERFLY = 50;
+
+let butterflyImages = [];
+
 /**
 Description of preload
 */
 function preload() {
+  for (let i = 0; i < BUTTERFLY_IMG; i++){
+    let butterflyImg = loadImage(`assets/images/butterfly${i}.png`);
+    butterflyImages.push(butterflyImg);
+  }
 }
 
 
