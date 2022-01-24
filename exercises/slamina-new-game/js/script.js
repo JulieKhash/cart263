46fe1d8fb5e138cc515ca1d9adc295c1b71885f2
Julie <gulshan.khashimova@gmail.com>
2022-01-24 18:47:17 -0500

    E2: started a slamina exercise

---
+++ exercises/slamina-new-game/js/script.js
@@ -0,0 +1,187 @@
+"use strict";
+
+const ANIMALS = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra",
+];
+
+let currentAnimal = "";
+let currentAnswer = "";
+
+function setup() {
+  createCanvas(600, 600);
+
+  if (annyang) {
+    let commands = {
+      "I think it is *ANIMAL": guessAnimal,
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+
+    textSize(40);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+  }
+}
+
+function draw() {
+  background(255, 100, 0);
+
+  if (currentAnswer === currentAnimal) {
+    fill(0, 255, 0);
+  } else {
+    fill(255, 0, 0);
+  }
+  text(currentAnswer, width / 2, height / 2);
+}
+
+function reverseString(string) {
+  let characters = string.split("");
+  let reverseCharacter = characters.reverse();
+  let result = reverseCharacter.join("");
+  return result;
+}
+
+function mousePressed() {
+  currentAnimal = random(ANIMALS);
+  let reverseAnimal = reverseString(currentAnimal);
+
+  responsiveVoice.speak(reverseAnimal);
+}
+
+function guessAnimal(animal) {
+  currentAnswer = animal.toLowerCase();
+  console.log(currentAnswer);
+}
