66c98177577f2615f8008e5759e88a0f6ff66f2e
Julie <gulshan.khashimova@gmail.com>
2022-01-28 17:10:00 -0500

    Voice experiments

---
+++ experiments/voice-exp/js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+let userSpeechArray = [
+  `What were you going to do`,
+  `Who are you`,
+  `I've come to answer your prayers`,
+  `Life has no meaning anymore does it`,
+  `How is it going`,
+];
+
+// current speech they are supposed to say
+let currentSpeech = ``;
+
+// text they are supposed to read
+let displayText = ``;
+
+function setup() {
+  createCanvas(800, 800);
+
+  if (annyang) {
+    let command = {
+      "*userSpeech": handleUserSpeech,
+    };
+    annyang.addCommands(command);
+    annyang.start();
+
+    // a phrase a user must say
+    userPhrase();
+  }
+}
+
+function draw() {
+  background(0);
+
+  textAlign(CENTER);
+  textSize(40);
+  fill(255, 0, 0);
+
+  //text(currentSpeech, width / 2, height / 2);
+  text(displayText, width / 2, height / 2);
+}
+
+function handleUserSpeech(userSpeech) {
+  if (userSpeech.toLowerCase() === currentSpeech.toLowerCase()) {
+    displayText = `Yes ${currentSpeech}`; // doesn't respond to the array of strings with punctuation
+  } else {
+    displayText = `again`;
+  }
+  console.log(userSpeech); // shows anything that user says on console
+}
+
+function userPhrase() {
+  currentSpeech = random(userSpeechArray);
+  displayText = `${currentSpeech}?`;
+
+  console.log(currentSpeech);
+}
