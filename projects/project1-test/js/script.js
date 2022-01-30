52acc053c88d9d24d12045b167315e9bc75319fb
Julie <gulshan.khashimova@gmail.com>
2022-01-29 21:58:05 -0500

    P1: added a speaking mirror, set up position and size

--- projects/project1-test/js/script.js
+++ projects/project1-test/js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+let mirrorFrameImg;
+let mirrorFrame = {
+  x: undefined,
+  y: undefined,
+  opacity: 200,
+};
+
+let flowerDropsImg;
+let flowerDrops = {
+  x: undefined,
+  y: undefined,
+  w: 400,
+  h: 500,
+};
+
 let libraryBWImg;
 let libraryColorImg;
 let libraryColor = {
@@ -9,6 +24,9 @@ let libraryColor = {
 function preload() {
   libraryBWImg = loadImage(`assets/images/libraryBW.png`);
   libraryColorImg = loadImage(`assets/images/libraryColored.png`);
+
+  flowerDropsImg = loadImage(`assets/images/flowerdrops.gif`);
+  mirrorFrameImg = loadImage(`assets/images/mirrorframe.png`);
 }
 
 function setup() {
@@ -25,7 +43,34 @@ function draw() {
   tint(255, libraryColor.opacity);
   image(libraryColorImg, width / 2, height / 2, 2600, 1500);
   pop();
+
+  speakingMirror();
 }
+
+function speakingMirror() {
+  let x = width / 2;
+  let y = height;
+
+  push();
+  flowerDrops.x = x;
+  flowerDrops.y = y + 50;
+  image(
+    flowerDropsImg,
+    flowerDrops.x,
+    flowerDrops.y,
+    flowerDrops.w,
+    flowerDrops.h
+  );
+  pop();
+
+  push();
+  mirrorFrame.x = x;
+  mirrorFrame.y = y;
+  tint(255, mirrorFrame.opacity);
+  image(mirrorFrameImg, mirrorFrame.x, mirrorFrame.y);
+  pop();
+}
+
 function mouseWheel(event) {
   if (event.delta > 0) {
     libraryColor.opacity += 100;
