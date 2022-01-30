0aad7c017de38555d0180b2f902092cd27388da5
Julie <gulshan.khashimova@gmail.com>
2022-01-29 21:42:58 -0500

    P1: added a bg picture added a basic mouse wheel event

--- projects/project1-test/js/script.js
+++ projects/project1-test/js/script.js
@@ -1,33 +1,38 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+let libraryBWImg;
+let libraryColorImg;
+let libraryColor = {
+  opacity: 1,
+};
 
-/**
-Description of preload
-*/
 function preload() {
-
+  libraryBWImg = loadImage(`assets/images/libraryBW.png`);
+  libraryColorImg = loadImage(`assets/images/libraryColored.png`);
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  image(libraryBWImg, width / 2, height / 2, 2600, 1500);
+
+  push();
+  tint(255, libraryColor.opacity);
+  image(libraryColorImg, width / 2, height / 2, 2600, 1500);
+  pop();
+}
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    libraryColor.opacity += 100;
+    libraryColor.opacity = constrain(libraryColor.opacity, 1, 255);
+  } else {
+    libraryColor.opacity -= 100;
+    libraryColor.opacity = constrain(libraryColor.opacity, 1, 0);
+  }
+  console.log(event.delta);
 }
