3bdf7d82ddc2fd897cccd2c8fda4af8716da0435
Julie <gulshan.khashimova@gmail.com>
2022-02-02 18:59:26 -0500

    P1: made the eye zoom in

--- projects/project1-test2/js/Eye.js
+++ projects/project1-test2/js/Eye.js
@@ -2,6 +2,10 @@ class Eye {
   constructor(imageEye) {
     this.x = width / 2;
     this.y = height / 2;
+    this.w = 500; //intial width
+    this.h = 281;
+    this.size = 10;
+
     this.opacity = 0;
 
     this.fadeRate = 10;
@@ -12,6 +16,14 @@ class Eye {
   update() {
     this.display();
     this.showEyeSlowly();
+    this.increaseEyeSlowly();
+  }
+
+  increaseEyeSlowly() {
+    if (bottleDrunken) {
+      this.w += 1.5;
+      this.h += 1;
+    }
   }
 
   showEyeSlowly() {
@@ -24,7 +36,7 @@ class Eye {
   display() {
     push();
     tint(255, this.opacity);
-    image(this.imageEye, this.x, this.y);
+    image(this.imageEye, this.x, this.y, this.w, this.h);
     pop();
   }
 }
