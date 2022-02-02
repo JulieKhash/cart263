a7da10bd899ba2791e62047118841e8e90655b04
Julie <gulshan.khashimova@gmail.com>
2022-02-01 20:00:17 -0500

    E3: added a user profile object with its properties

--- exercises/spy-profile-generator/js/script.js
+++ exercises/spy-profile-generator/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Past Life Generator
+Julie Khashimova
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,39 @@ author, and this description to match your project!
 
 "use strict";
 
+let userProfile = {
+  name: "HIDDEN",
+  homeLand: "HIDDEN",
+  bodyPart: "HIDDEN",
+  description: "HIDDEN",
+  object: "HIDDEN",
+  food: "HIDDEN",
+  mood: "HIDDEN",
+};
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  push();
+  textAlign(LEFT, TOP);
+  textFont(`Courier`);
+  textSize(40);
+  fill(0, 100, 255);
+  text(profile, 100, 100);
+  pop();
 }
