949425335ac4a20f15b2c3c78ae851d3fe3b4da1
Julie <gulshan.khashimova@gmail.com>
2022-02-08 17:24:48 -0500

    E3: basic program set up

--- exercises/bubble-popper-new/js/script.js
+++ exercises/bubble-popper-new/js/script.js
@@ -7,27 +7,26 @@ author, and this description to match your project!
 */
 
 "use strict";
-
+let cleaningClothImg;
+let inkImg;
+let inkspots = []; //an empty array to store the ink spots
 
 /**
 Description of preload
 */
 function preload() {
-
+  cleaningClothImg = loadImage("assets/images/cloth.png");
+  inkImg = loadImage("assets/images/spot.png");
 }
-
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
-
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
