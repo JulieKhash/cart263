d820dd1a87f033f64d707b264c095636f9215432
Julie <gulshan.khashimova@gmail.com>
2022-02-08 17:31:52 -0500

    E4: declared variables

--- exercises/bubble-popper-new/js/script.js
+++ exercises/bubble-popper-new/js/script.js
@@ -9,7 +9,22 @@ author, and this description to match your project!
 "use strict";
 let cleaningClothImg;
 let inkImg;
-let inkspots = []; //an empty array to store the ink spots
+let inkspots = []; //an empty array to store the inkspots
+
+let inkspots; // ink spots we will be cleaning :)
+let inkspotsCleaned = false; // initially not cleaned
+
+let cleaningCloth = {
+  x: undefined,
+  y: undefined,
+  size: 400,
+};
+
+let state = `loading`; // current state of the program while loading
+let video; // user's webcam
+let modelName = `Handpose`; //the name of our model
+let handpose; // handpose object
+let predictions = []; // the current set of predictions made by handpose
 
 /**
 Description of preload
