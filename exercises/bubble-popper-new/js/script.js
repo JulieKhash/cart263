271baefa768c53ecda9bdf74c56fd5cc743eb383
Julie <gulshan.khashimova@gmail.com>
2022-02-08 19:58:21 -0500

    E4: made a hand appear on screen , created an osc

--- exercises/bubble-popper-new/js/script.js
+++ exercises/bubble-popper-new/js/script.js
@@ -7,22 +7,15 @@ author, and this description to match your project!
 */
 
 "use strict";
-let handLowImg;
-let handMidImg;
-let handHighImg;
-let handimages = []; // to store an hand images
 
-let inkspot; // ink spot we will be cleaning :)
-let inkspotsCleaned = false; // initially not cleaned
-
-let starImg;
-let star;
-
-let cleaningCloth = {
-  x: undefined,
-  y: undefined,
-  size: 200,
+let handImg;
+let handImages = []; // to store an hand images
+let handParameters = {
+  w: 200,
+  h: 160,
 };
+
+let oscillator;
 
 let state = `running`; // current state of the program while loading
 let video; // user's webcam
@@ -35,7 +28,8 @@ Description of preload
 */
 function preload() {
   for (let i = 0; i < 2; i++) {
-    handImages[i] = loadImage(`assets/images/hand${i}.png`);
+    handImg = loadImage(`assets/images/hand${i}.png`);
+    handImages.push(handImg);
   }
 }
 
@@ -43,8 +37,15 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 600);
   background(0);
+
+  userStartAudio();
+  oscillator = new p5.Oscillator(`sine`); // create a sine wave
+
+  oscillator.start();
+
+  // oscillator.start();
 
   // start a webcam and hide the resulting html element
   video = createCapture(VIDEO);
@@ -71,8 +72,6 @@ function draw() {
     loading();
   } else if (state === `running`) {
     running();
-  } else if (state === `cleaned`) {
-    cleaned();
   }
 }
 
@@ -80,21 +79,35 @@ function running() {
   // check if there predictions to be made
   if (predictions.length > 0) {
     let hand = predictions[0]; // there's only one hand cuz it detecs only one hand
-    showCleaningCloth(hand);
+    let middleFinger = hand.annotations.middleFinger;
+
+    let middleFingerTip = middleFinger[3];
+    // let middleFingerBase = middleFinger[0]
+
+    image(
+      handImages[0],
+      middleFingerTip[0],
+      middleFingerTip[1],
+      handParameters.w,
+      handParameters.h
+    );
+    // showHands(hand);
+    console.log(hand);
+    // oscillator.start();
   }
 }
 
-function showCleaningCloth(hand) {
-  let index = hand.annotations.indexFinger[3];
-  let indexX = index[0];
-  let indexY = index[1];
+// function showHands(hand) {
+//   push();
+//   handX = hand.annotations.palmBase[0];
+//   // handY = hand.middleFinger[3];
+//   image(handImages[0], handX, handX);
+//   pop();
+// }
+
+function loading() {
+  background(255);
   push();
-  image(
-    cleaningClothImg,
-    indexX,
-    indexY,
-    cleaningCloth.size,
-    cleaningCloth.size
-  );
+  text(`Loading...`, widht / 2, height / 2);
   pop();
 }
