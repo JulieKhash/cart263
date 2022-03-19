a674084b8f6aad9ceac8313164ccef31ed59d1ae
Julie <gulshan.khashimova@gmail.com>
2022-03-19 17:39:39 -0400

    P2: added a basic launch pad structure

--- projects/project2-prototype/js/script.js
+++ projects/project2-prototype/js/script.js
@@ -1,8 +1,1 @@
 "use strict";
-
-const music = new Audio(`assets/sounds/13_RitualDjembe.wav`);
-
-$("#sample1-button").button({ classes: { "ui-button": "highlight" } });
-$(this).click(function () {
-  music.play();
-});
