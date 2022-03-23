596eab921ac841961c4cb9bb4bee3d69a6873a57
Julie <gulshan.khashimova@gmail.com>
2022-03-22 22:12:25 -0400

    P2: added a branch and some animation effetcs

--- projects/project2-prototype2/js/script.js
+++ projects/project2-prototype2/js/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // const numCrystals = 5;
-
 // for (let i = 0; i < numCrystals; i++) {
 $(`body`).append(
   `<img class="crystal-img" id="crystal1" src="assets/images/crystal2.png">`
@@ -13,23 +12,33 @@ $(`body`).append(
 $(`body`).append(
   `<img class="crystal-img" id="crystal3" src="assets/images/crystal3.png">`
 );
-// }
+
+$(`body`).append(`<img id="background" src="assets/images/branch.png">`);
+
+let $text = $(`p`).append(`drop them onto the circle`);
 
 let $crystals = $(`.crystal-img`);
 let $crystal1 = $(`#crystal1`);
 let $crystal2 = $(`#crystal2`);
 let $crystal3 = $(`#crystal3`);
 
+let $branch = $(`#background`);
+
 let crystalSound = new Audio(`assets/sounds/crystalcave.wav`);
 let crystalSound2 = new Audio(`assets/sounds/glassy.wav`);
 let crystalSound3 = new Audio(`assets/sounds/steeltrap.wav`);
+
+$text.effect({
+  effect: `pulsate`,
+  duration: 10000,
+});
 
 // make the
 $crystals.draggable();
 
 $crystal1.on(`click`, function () {
   crystalSound.play();
-  crystalSound.volume(0.1);
+  crystalSound.volume = 0.2;
 });
 
 $crystal2.on(`click`, function () {
@@ -39,25 +48,31 @@ $crystal2.on(`click`, function () {
 
 $crystal3.on(`click`, function () {
   crystalSound3.play();
-  crystalSound3.volume(0.5);
+  crystalSound.volume = 0.2;
 });
-
-// $crystals.on(`click`, function () {
-//   crystalSound.play();
-//   crystalSound.volume(0.5);
-// });
 
 $(`#mixer-box`).droppable({
   drop: function (event, ui) {
+    // just testting
+    // the idea is to play the sound specific to the crystal dropped into the circle and keep it looping
+
     crystalSound.play();
     crystalSound.volume = 0.2;
     crystalSound.loop = true;
-    ui.draggable.css({ opacity: 1 });
 
-    // make it smaller
-    ui.draggable.resizable({
-      animate: true,
-    });
+    crystalSound2.play();
+    crystalSound.volume = 0.2;
+    crystalSound.loop = true;
+
+    crystalSound3.play();
+    crystalSound.volume = 0.2;
+    crystalSound.loop = true;
+
+    $text.hide();
+
+    ui.draggable.animate({ opacity: 1 });
+    $(this).animate({ opacity: 1 }, 1000);
+    $branch.animate({ opacity: 1 }, 3000);
   },
 });
 
