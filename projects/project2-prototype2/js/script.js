df7c6e3f54ea3f8589601a5db022bf0742300817
Julie <gulshan.khashimova@gmail.com>
2022-03-22 22:39:46 -0400

    P2: added some comments

--- projects/project2-prototype2/js/script.js
+++ projects/project2-prototype2/js/script.js
@@ -1,60 +1,78 @@
 "use strict";
+/**
+Project 2 - prototype (very rough one!)
+Julie Khashimova
 
+Drag and drop "crystals" onto the circle (sound mixer).
+Makes the smoke appear and plays the sound. That's it haha
+*/
+
+// may want to multiply the number of crystals
 // const numCrystals = 5;
 // for (let i = 0; i < numCrystals; i++) {
+
+// appends image elemetns into the html canvas
 $(`body`).append(
   `<img class="crystal-img" id="crystal1" src="assets/images/crystal2.png">`
 );
 $(`body`).append(
   `<img class="crystal-img" id="crystal2" src="assets/images/crystal.png">`
 );
-
 $(`body`).append(
   `<img class="crystal-img" id="crystal3" src="assets/images/crystal3.png">`
 );
 
-$(`body`).append(`<img id="background" src="assets/images/branch.png">`);
+$(`body`).append(`<img id="background" src="assets/images/smoke.png">`);
 
-let $text = $(`p`).append(`drop them onto the circle`);
+// appends text
+let $text = $(`p`).append(`drag and drop onto the circle`);
 
+// variables for specific crystals
 let $crystals = $(`.crystal-img`);
 let $crystal1 = $(`#crystal1`);
 let $crystal2 = $(`#crystal2`);
 let $crystal3 = $(`#crystal3`);
 
-let $branch = $(`#background`);
+// smoke image
+let $smoke = $(`#background`);
 
+// loads sounds
 let crystalSound = new Audio(`assets/sounds/crystalcave.wav`);
 let crystalSound2 = new Audio(`assets/sounds/glassy.wav`);
 let crystalSound3 = new Audio(`assets/sounds/steeltrap.wav`);
 
+// gives a text a pulsating effect
 $text.effect({
   effect: `pulsate`,
   duration: 10000,
 });
 
-// make the
+// make the crystals draggable
 $crystals.draggable();
 
+// plays a specific sound to the crystal
 $crystal1.on(`click`, function () {
   crystalSound.play();
-  crystalSound.volume = 0.2;
+  crystalSound.volume = 0.1;
 });
 
 $crystal2.on(`click`, function () {
   crystalSound2.play();
-  crystalSound.volume = 0.2;
+  crystalSound.volume = 0.1;
 });
 
 $crystal3.on(`click`, function () {
   crystalSound3.play();
-  crystalSound.volume = 0.2;
+  crystalSound.volume = 0.1;
 });
 
+// dropping circle (should serve as a dropping for the sound crystals)
 $(`#mixer-box`).droppable({
   drop: function (event, ui) {
-    // just testting
-    // the idea is to play the sound specific to the crystal dropped into the circle and keep it looping
+    // just testing here
+    // the idea is to play the sound specific to the crystal dropped into the circle
+    // and keep it looping as long as its there
+    // will find a better way/solution to separate the sounds once the crystal is dropped on the circle
 
     crystalSound.play();
     crystalSound.volume = 0.2;
@@ -68,19 +86,19 @@ $(`#mixer-box`).droppable({
     crystalSound.volume = 0.2;
     crystalSound.loop = true;
 
+    // should hide the test when user starts dropping the crystals onto circl but it still shows up in the end
     $text.hide();
 
     ui.draggable.animate({ opacity: 1 });
     $(this).animate({ opacity: 1 }, 1000);
-    $branch.animate({ opacity: 1 }, 3000);
+    $smoke.animate({ opacity: 1 }, 3000);
   },
 });
 
-// add random positions
-// play all sounds att the same time
-// add a different type of sound (another crystal variation)
-// draggable can be removed out of the box?
-// draggable change size / or opacity when is inside the cicle
+// TO DO NEXT:
 
-// multiply the same image = put it inside html or js
-// sound library? for looping and sound effects
+// add random positions to crystals (or some other sound pieces);
+// improve the functionality of draggable/droppable(draggable can be removed out of the box?)
+// set up an appropriate positions for DOM elements
+// play only the sounds that are dropped on the circle
+//... many more
