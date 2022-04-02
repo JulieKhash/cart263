f67e2f7f5d40eea0facf1789cf93e444c04d22b6
Julie <gulshan.khashimova@gmail.com>
2022-04-01 23:52:31 -0400

    P2: made the variables for all the images

--- projects/project2-test/js/script.js
+++ projects/project2-test/js/script.js
@@ -1,13 +1,16 @@
 "use strict";
-let $tree = `<img id="tree"src="assets/images/tree-cut.png">`;
-let $figure = `<img id="face"src="assets/images/moth-face.png">`;
-let $ghost = `<img id="ghost"src="assets/images/fire2.png">`;
-let $walker = `<img id="walker"src="assets/images/rope-walker.png">`;
-let $sun = `<img id="sun"src="assets/images/wingman4.png">`;
-let $moth = `<img id="moth"src="assets/images/sun2.png">`;
-let $swing = `<img id="swing"src="assets/images/swing1.png">`;
-let $smoke = `<img id="smoke"src="assets/images/smoke.png">`;
-let $city = `<img id="city"src="assets/images/darkplanet.png">`;
+let $tree = `<img id="tree"src="assets/images/tree-cut.png">`; //1
+let $mothEye = `<img id="face"src="assets/images/moth-face.png">`; //2
+let $fire = `<img id="ghost"src="assets/images/fire.png">`; //3
+let $walker = `<img id="walker"src="assets/images/rope-walker-shadow2.png">`; //4
+let $wingedMan = `<img id="sun"src="assets/images/wingman4.png">`; //5
+let $sun = `<img id="moth"src="assets/images/sun2.png">`; //6
+let $swingFigure = `<img id="swing"src="assets/images/swing1.png">`; //7
+let $skyBg = `<img id="smoke"src="assets/images/sea2.jpg">`; //8
+let $darkPlanet = `<img id="city"src="assets/images/darkplanet.png">`; //9
+let $smoke = `<img id="city"src="assets/images/smoke.png">`; //10
+let $whiteMoth = `<img id="city"src="assets/images/mothwhite.png">`; //11
+let $crystal = `<img id="city"src="assets/images/crystal1.png">`; //12
 
 // sound
 let crystalSound = new Audio(`assets/sounds/crystalcave.wav`);
@@ -20,58 +23,58 @@ let $scene1 = `<p class="scenes-text" id="scene-1"> I had this dream...</p>`;
 let $scene2 = `<p class="scenes-text" id="scene-2">back on the Earth where our ancestors walked,
  before a curtain fell between the worlds, before we began to forget...</p>`;
 
-$(`#text1`).one(`click`, function () {
-  $(`body`).append($tree);
-  $(`#tree`).animate({ opacity: 0.7 }, 6000);
-});
-
-$(`#text2`).one(`click`, function () {
-  $(`body`).append($figure);
-  $(`body`).append($scene1);
-  $(`#face`).animate({ opacity: 0.9 }, 3000);
-  $(`#scene-1`).animate({ opacity: 0.9 }, 6000);
-  crystalSound.play();
-  crystalSound.volume = 0.2;
-  crystalSound.loop = true;
-});
-
-$(`#text3`).one(`click`, function () {
-  $(`body`).append($ghost);
-  $(`#ghost`).animate({ opacity: 0.9 }, 6000);
-});
-
-$(`#text4`).one(`click`, function () {
-  $(`body`).append($walker);
-  $(`body`).append($scene2);
-  $(`#walker`).animate({ opacity: 0.9 }, 6000);
-  $(`#scene-2`).animate({ opacity: 0.9 }, 8000);
-});
-
-$(`#text5`).one(`click`, function () {
-  $(`body`).append($sun);
-  $(`#sun`).animate({ opacity: 0.9 }, 6000);
-});
-
-$(document).on(`click`, function () {
-  $(`#moth`).effect({ effect: `pulsate`, duration: 1000 }, 500);
-});
-
-$(`#text6`).one(`click`, function () {
-  $(`body`).append($moth);
-  $(`#moth`).animate({ opacity: 1 }, 6000);
-});
-
-$(`#text7`).one(`click`, function () {
-  $(`body`).append($swing);
-  $(`#swing`).animate({ opacity: 1 }, 6000);
-});
-
-$(`#text8`).one(`click`, function () {
-  $(`body`).append($smoke);
-  $(`#smoke`).animate({ opacity: 1 }, 6000);
-});
-
-$(`#text9`).one(`click`, function () {
-  $(`body`).append($city);
-  $(`#city`).animate({ opacity: 1 }, 6000);
-});
+// $(`#text1`).one(`click`, function () {
+//   $(`body`).append($tree);
+//   $(`#tree`).animate({ opacity: 0.7 }, 6000);
+// });
+//
+// $(`#text2`).one(`click`, function () {
+//   $(`body`).append($figure);
+//   $(`body`).append($scene1);
+//   $(`#face`).animate({ opacity: 0.9 }, 3000);
+//   $(`#scene-1`).animate({ opacity: 0.9 }, 6000);
+//   crystalSound.play();
+//   crystalSound.volume = 0.2;
+//   crystalSound.loop = true;
+// });
+//
+// $(`#text3`).one(`click`, function () {
+//   $(`body`).append($ghost);
+//   $(`#ghost`).animate({ opacity: 0.9 }, 6000);
+// });
+//
+// $(`#text4`).one(`click`, function () {
+//   $(`body`).append($walker);
+//   $(`body`).append($scene2);
+//   $(`#walker`).animate({ opacity: 0.9 }, 6000);
+//   $(`#scene-2`).animate({ opacity: 0.9 }, 8000);
+// });
+//
+// $(`#text5`).one(`click`, function () {
+//   $(`body`).append($sun);
+//   $(`#sun`).animate({ opacity: 0.9 }, 6000);
+// });
+//
+// $(document).on(`click`, function () {
+//   $(`#moth`).effect({ effect: `pulsate`, duration: 1000 }, 500);
+// });
+//
+// $(`#text6`).one(`click`, function () {
+//   $(`body`).append($moth);
+//   $(`#moth`).animate({ opacity: 1 }, 6000);
+// });
+//
+// $(`#text7`).one(`click`, function () {
+//   $(`body`).append($swing);
+//   $(`#swing`).animate({ opacity: 1 }, 6000);
+// });
+//
+// $(`#text8`).one(`click`, function () {
+//   $(`body`).append($smoke);
+//   $(`#smoke`).animate({ opacity: 0.8 }, 6000);
+// });
+//
+// $(`#text9`).one(`click`, function () {
+//   $(`body`).append($city);
+//   $(`#city`).animate({ opacity: 1 }, 6000);
+// });
