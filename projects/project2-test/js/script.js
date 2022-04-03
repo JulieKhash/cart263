fcd51d3b70735886b8b02ec5b7bdd516b262aa19
Julie <gulshan.khashimova@gmail.com>
2022-04-03 00:39:14 -0400

    P2: added a basic image toggle, issues with its proper functionality

--- projects/project2-test/js/script.js
+++ projects/project2-test/js/script.js
@@ -2,12 +2,12 @@
 
 let $tree = `<img class="images"id="tree"src="assets/images/tree-cut.png">`; //1
 let $mothEye = `<img class="images" id="mothEye"src="assets/images/moth-face.png">`; //2
-let $fire = `<img class="images"id="fire"src="assets/images/fire4.png">`; //3
+let $fire = `<img class="images"id="fire"src="assets/images/fire5.png">`; //3
 let $walker = `<img class="images"id="walker"src="assets/images/rope-walker-shadow2.png">`; //4
-let $wingedMan = `<img class="images"id="wingedMan"src="assets/images/wingman4.png">`; //5
+let $wingedMan = `<img class="images"id="wingedMan"src="assets/images/wingmanC.png">`; //5
 let $sun = `<img class="images"id="sun"src="assets/images/sun2.png">`; //6
 let $swingFigure = `<img class="images"id="swingFigure"src="assets/images/swing1.png">`; //7
-let $skyBg = `<img class="images"id="skyBg"src="assets/images/sea2.jpg">`; //8
+let $skyBg = `<img class="images"id="skyBg"src="assets/images/skybg3.png">`; //8
 let $darkPlanet = `<img class="images"id="darkPlanet"src="assets/images/darkplanet.png">`; //9
 let $smoke = `<img class="images"id="smoke"src="assets/images/smoke.png">`; //10
 let $whiteMoth = `<img class="images"id="whiteMoth"src="assets/images/mothwhite.png">`; //11
@@ -25,6 +25,17 @@ let crystalSound3 = new Audio(`assets/sounds/steeltrap.wav`);
 //  before a curtain fell between the worlds, before we began to forget...</p>`;
 
 $(`#image-container`).addClass(`.images`);
+let $images = $(`.images`);
+
+// $images.each(fadeout);
+//
+// function hideImg() {
+//   setTimeout(() => $(`#image-container`).removeClass(`.images`), 2000);
+// }
+
+function fadeout() {
+  setTimeout(() => $(`#tree`).animate({ opacity: 0 }, 2000));
+}
 
 $(`#icon1`).one(`click`, function () {
   $(`#image-container`).append($tree);
@@ -48,6 +59,7 @@ $(`#icon5`).one(`click`, function () {
 
 $(`#icon6`).one(`click`, function () {
   $(`#image-container`).append($sun);
+  $(`$sun`).effect(`pulsate`);
 });
 
 $(`#icon7`).one(`click`, function () {
@@ -78,67 +90,10 @@ $(`#icon12`).one(`click`, function () {
 //   $(`#image-container`).append($flame);
 // });
 
-$(`.icons`).one(`click`, function () {
-  $(`.images`).animate({ opacity: 0.8 }, 2000);
+// click handler
+$(`.icons`).on(`click`, function () {
+  $(`.images`).animate({ opacity: 0.8 }, 3000);
+  setTimeout(() => $(`.images`).animate({ opacity: 0 }, 3000));
+  // $(`.images`).each(fadeout);
   console.log(`cliicked`);
 });
-
-// $(`.images`).animate({ opacity: 0.7 }, 1000);
-// $(`#tree`).animate({ opacity: 0.7 }, 6000);
-// console.log(`cliicked`);
-
-// $(`#text1`).one(`click`, function () {
-//   $(`body`).append($tree);
-//   $(`#tree`).animate({ opacity: 0.7 }, 6000);
-// });
-//
-// $(`#text2`).one(`click`, function () {
-//   $(`body`).append($figure);
-//   $(`body`).append($scene1);
-//   $(`#face`).animate({ opacity: 0.9 }, 3000);
-//   $(`#scene-1`).animate({ opacity: 0.9 }, 6000);
-//   crystalSound.play();
-//   crystalSound.volume = 0.2;
-//   crystalSound.loop = true;
-// });
-//
-// $(`#text3`).one(`click`, function () {
-//   $(`body`).append($ghost);
-//   $(`#ghost`).animate({ opacity: 0.9 }, 6000);
-// });
-//
-// $(`#text4`).one(`click`, function () {
-//   $(`body`).append($walker);
-//   $(`body`).append($scene2);
-//   $(`#walker`).animate({ opacity: 0.9 }, 6000);
-//   $(`#scene-2`).animate({ opacity: 0.9 }, 8000);
-// });
-//
-// $(`#text5`).one(`click`, function () {
-//   $(`body`).append($sun);
-//   $(`#sun`).animate({ opacity: 0.9 }, 6000);
-// });
-//
-// $(document).on(`click`, function () {
-//   $(`#moth`).effect({ effect: `pulsate`, duration: 1000 }, 500);
-// });
-//
-// $(`#text6`).one(`click`, function () {
-//   $(`body`).append($moth);
-//   $(`#moth`).animate({ opacity: 1 }, 6000);
-// });
-//
-// $(`#text7`).one(`click`, function () {
-//   $(`body`).append($swing);
-//   $(`#swing`).animate({ opacity: 1 }, 6000);
-// });
-//
-// $(`#text8`).one(`click`, function () {
-//   $(`body`).append($smoke);
-//   $(`#smoke`).animate({ opacity: 0.8 }, 6000);
-// });
-//
-// $(`#text9`).one(`click`, function () {
-//   $(`body`).append($city);
-//   $(`#city`).animate({ opacity: 1 }, 6000);
-// });
